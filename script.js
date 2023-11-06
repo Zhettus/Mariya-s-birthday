ef066601711716a054d8b7df4855c8d843b7c6b5
Zhettus <t_zhadyra_t@mail.ru>
2023-11-06 13:21:59 +0600

    text changes

--- script.js
+++ script.js
@@ -3,3 +3,26 @@ document.getElementById('scroll-down').addEventListener('click', function() {
     infoSection.style.display = 'block';
     infoSection.scrollIntoView({ behavior: 'smooth' });
 });
+// Function to handle animations when an element enters the viewport
+function handleAnimations(entries, observer) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('animate');
+        observer.unobserve(entry.target);
+      }
+    });
+  }
+  
+  // Create an Intersection Observer
+  const observer = new IntersectionObserver(handleAnimations, {
+    threshold: 0.5, // Adjust this threshold as needed
+  });
+  
+  // Get all elements with the 'animate' class
+  const animatedElements = document.querySelectorAll('.animate');
+  
+  // Observe each animated element
+  animatedElements.forEach((element) => {
+    observer.observe(element);
+  });
+  
